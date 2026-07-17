e4187b0259c6cb3133d63b8716933f7ead161f07
Arghajit Das <arghajit2006@gmail.com>
2026-07-17 23:48:21 +0530

    Add bubble sort implementation in C
    Implemented bubble sort algorithm and added user input functionality.

--- Sorting/Bubble_Sort/bubble_sort.c
+++ Sorting/Bubble_Sort/bubble_sort.c
@@ -1,1 +1,44 @@
-
+#include <stdio.h>
+void SWAP(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+void BUBBLE_SORT(int array[], int size){
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (array[j]>array[j+1])
+            {
+                SWAP(&array[j], &array[j+1]);
+            }
+            
+        }
+        
+    }
+    
+}
+int main(){
+    int array[500],  limit;
+    printf("Enter Array Limmit\n");
+    scanf("%d", &limit);
+    for (int i = 0; i < limit; i++)
+    {
+        printf("INDEX[%d]\n", i);
+        scanf("%d", &array[i]);
+    }
+    for (int i = 0; i < limit; i++)
+    {
+        printf("%d\t", array[i]);
+        
+    }
+    BUBBLE_SORT(array, limit);
+    printf("\nAfter Applying Bubble Sort\n");
+    for (int i = 0; i < limit; i++)
+    {
+        printf("%d\t", array[i]);
+        
+    }
+    return 0;
+}
