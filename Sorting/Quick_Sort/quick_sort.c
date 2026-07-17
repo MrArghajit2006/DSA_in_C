b11bf58661f0539c1b3e9b435adf8589b26e64a8
Arghajit Das <arghajit2006@gmail.com>
2026-07-17 23:51:57 +0530

    Add Quick Sort implementation in quick_sort.c
    Implement Quick Sort algorithm with partitioning and swapping functions.

--- Sorting/Quick_Sort/quick_sort.c
+++ Sorting/Quick_Sort/quick_sort.c
@@ -1,1 +1,40 @@
+#include <stdio.h>
+#define MAX 6
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int partition(int arr[], int start, int end){
+    int pivot = arr[end];
+    int i = start-1;
+    for (int j = start; j <= end-1; j++)
+    {
+        if (pivot >= arr[j])
+        {
+            i = i+1;
+            swap(&arr[i],&arr[j]);
+        }
+    }
+    swap(&arr[i+1], &arr[end]);
+    return i+1;
+}
+void quick_sort(int arr[], int start, int end){
+    if (start<end)
+    {
+        int mid = partition(arr, start, end);
+        quick_sort(arr, start, mid-1);
+        quick_sort(arr, mid+1, end);
+    }
+}
+int main(){
+    int array[] = {10,30,40,50,70,90,80};
+    quick_sort(array, 0, 6);
+    for (int i = 0; i < 7; i++)
+    {
+        printf("%d ", array[i]);
+    }
+    
 
+    return 0;
+}
