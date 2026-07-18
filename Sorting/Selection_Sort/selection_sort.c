09e6571ca134ac2b08d6f2c2ae179657863d5eec
Arghajit Das <arghajit2006@gmail.com>
2026-07-18 21:32:35 +0530

    Implement selection sort and input/output functions
    Fix selection sort implementation and improve input handling.

--- Sorting/Selection_Sort/selection_sort.c
+++ Sorting/Selection_Sort/selection_sort.c
@@ -1,1 +1,46 @@
-
+#include <stdio.h>
+int array[100];
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+void SELECTION_SORT(int end){
+    for (int i = 1; i <= end-1; i++)
+    {
+        int min = array[i];
+        for (int j = i+1; j <= end; j++)
+        {
+            if (array[j]< min)
+            {
+                min = array[j];
+            }
+            
+        }
+        if (array[i] != min)
+        {
+            swap(&array[i], &min);
+        }
+    }
+}
+int main(){
+    int limit;
+    printf("Enter Array limit:: \n");
+    scanf("%d", &limit);
+    for (int i = 1; i <= limit; i++)
+    {
+        scanf("%d", &array[i]);
+    }
+    printf("Entered Array\n");
+    for (int i = 1; i <= limit; i++)
+    {
+        printf("%d\t", array[i]);
+    }
+    SELECTION_SORT(limit);
+    printf("\nAfter Sorting\n");
+    for (int i = 1; i <= limit; i++)
+    {
+        printf("%d\t", array[i]);
+    }
+    return 0;
+}
